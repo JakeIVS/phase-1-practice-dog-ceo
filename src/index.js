0b4c73b3d23d19089d089d3884bade2f09cacf17
Jacob Ives <jacob.w.ives@gmail.com>
2023-05-09 14:01:43 -0600

    complete challenge 2

--- src/index.js
+++ src/index.js
@@ -7,7 +7,19 @@ document.addEventListener('DOMContentLoaded',()=>{
             const img = document.createElement('img')
             const imageContainer = document.querySelector("#dog-image-container")
             img.src = image;
+            img.style.width = '25%'
             imageContainer.appendChild(img);
         })
+    })
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(res=>res.json())
+    .then(data=>{
+        let breeds = data.message;
+        for (let breed in breeds) {
+            let li= document.createElement('li');
+            let ul= document.querySelector('#dog-breeds')
+            li.textContent = breed;
+            ul.appendChild(li);
+        }
     })
 })
