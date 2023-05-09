007d15a481620284dbc661c0a8ff3bb194af35ff
Jacob Ives <jacob.w.ives@gmail.com>
2023-05-09 13:48:15 -0600

    complete challenge 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded',()=>{
+    fetch ('https://dog.ceo/api/breeds/image/random/4')
+    .then(r=>r.json())
+    .then((data)=> {
+        data.message.forEach((image)=>{
+            const img = document.createElement('img')
+            const imageContainer = document.querySelector("#dog-image-container")
+            img.src = image;
+            imageContainer.appendChild(img);
+        })
+    })
+})
